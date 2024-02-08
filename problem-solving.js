7183766a09e1d6b477e9e7e423a2467f557dff20
Lamiasyed <lamia.syed176@gmail.com>
2024-02-08 09:01:26 +0600

    problem 3,4 solved

--- problem-solving.js
+++ problem-solving.js
@@ -1,21 +1,75 @@
-// problems1
-function calculateMoney(ticketSale){
+// problem-1
+// function calculateMoney(ticketSale){
 
-    if(ticketSale < 1) {
-        return 'Please your validition numbers'
-    }  
+//     if(ticketSale < 1) {
+//         return 'Book your ticket now';
+//     }  
  
-  else{
-    const perPiceTicketSale = 120;
-    const darawanCost = 500;
-    const lunchFee = 50*8;
+//   else{
+//     const perPieceTicketSale = 120;
+//     const darowanCost = 500;
+//     const lunchFee = 50*8;
 
-    let totalRecievedAmount = perPiceTicketSale * parseInt(ticketSale);
-    const totalCost = darawanCost + lunchFee
+//     let totalRecievedAmount = perPieceTicketSale * parseInt(ticketSale);
+//     const totalCost = darowanCost + lunchFee
 
-    const totalProfit = totalRecievedAmount - totalCost;
-    return totalProfit 
-  }
-   
-   
+//     const totalProfit = totalRecievedAmount - totalCost;
+//     return totalProfit ;
+//   }
+//    console.log(totalProfit)
+// }
+
+
+
+
+
+
+// // problem-2
+// function checkName(name){
+
+//     if(typeof(name) !== 'string') return "Invalid"
+
+//     const validCharList = ['A', 'Y', 'I' , 'E' , 'O' , 'U', 'W'] 
+//     const lastCharOfName = name.charAt(name.length -1)
+
+//     const charExist = validCharList.filter(item => item.toLowerCase() === lastCharOfName.toLowerCase())
+
+//     if(charExist.length > 0) return 'Good Name'
+//     return 'Bad Name'
+ 
+// }
+
+
+// problem-3
+
+function deleteInvalids(Array) {
+    function checkArray(value) {
+        return typeof value === 'number' && !isNaN(value);
+    }
+
+    const result = [];
+    for (let i = 0; i < Array.length; i++) {
+        if (checkArray(Array[i])) {
+            result.push(Array[i]);
+        }
+    }
+    return result;
 }
+const userArray = [NaN, 1, 12, 0, -1, undefined];
+const arrayResult = deleteInvalids(userArray);
+console.log(arrayResult); 
+
+
+// // problem- 4
+// function password(object) {
+//     const password = object.siteName + object.name + object.birthYear;
+//     return password;     
+// }
+
+// const Info = { 
+//     name: "kolimuddin", 
+//     birthYear: 1999,
+//      siteName: "google" 
+//     };
+// const makingPass = password(Info); 
+// console.log(makingPass)
