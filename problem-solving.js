bd2daae93e9be08c807c169b1321d5f60d184d5d
Lamiasyed <lamia.syed176@gmail.com>
2024-02-08 21:16:24 +0600

    processing

--- problem-solving.js
+++ problem-solving.js
@@ -1,22 +1,22 @@
-// problem-1
+// // problem-1
 // function calculateMoney(ticketSale){
 
 //     if(ticketSale < 1) {
-//         return 'Book your ticket now';
+//         return 'Enter validation number';
 //     }  
  
 //   else{
-//     const perPieceTicketSale = 120;
-//     const darowanCost = 500;
+//     const perPiceTicketSale = 120;
+//     const guardCost = 500;
 //     const lunchFee = 50*8;
 
-//     let totalRecievedAmount = perPieceTicketSale * parseInt(ticketSale);
-//     const totalCost = darowanCost + lunchFee
+//     let totalRecievedAmount = perPiceTicketSale * parseInt(ticketSale);
+//     const totalCost = guardCost + lunchFee
 
 //     const totalProfit = totalRecievedAmount - totalCost;
 //     return totalProfit ;
 //   }
-//    console.log(totalProfit)
+   
 // }
 
 
@@ -24,40 +24,45 @@
 
 
 
-// // problem-2
+
+
+
+
+
+
+// // // problem-2
 // function checkName(name){
 
-//     if(typeof(name) !== 'string') return "Invalid"
-
-//     const validCharList = ['A', 'Y', 'I' , 'E' , 'O' , 'U', 'W'] 
-//     const lastCharOfName = name.charAt(name.length -1)
-
+//     if(typeof(name) !== 'string') 
+//     return "Invalid";
+//     const validCharList = ['a', 'y', 'i' , 'e' , 'o' , 'u', 'w'] 
+//     const lastCharOfName = name.charAt(name.length-1)
 //     const charExist = validCharList.filter(item => item.toLowerCase() === lastCharOfName.toLowerCase())
-
-//     if(charExist.length > 0) return 'Good Name'
-//     return 'Bad Name'
+//     if(charExist.length > 0) 
+//     return 'Good Name';
+//     return 'Bad Name';
  
 // }
 
 
 // problem-3
 
-function deleteInvalids(Array) {
-    function checkArray(value) {
-        return typeof value === 'number' && !isNaN(value);
-    }
+// function deleteInvalids(Array) {
+//     function checkArray(value) {
+//         return typeof value === 'number' && !isNaN(value);
+//     }
 
-    const result = [];
-    for (let i = 0; i < Array.length; i++) {
-        if (checkArray(Array[i])) {
-            result.push(Array[i]);
-        }
-    }
-    return result;
-}
-const userArray = [NaN, 1, 12, 0, -1, undefined];
-const arrayResult = deleteInvalids(userArray);
-console.log(arrayResult); 
+//     const result = [];
+//     for (let i = 0; i < Array.length; i++) {
+//         if (checkArray(Array[i])) {
+//             result.push(Array[i]);
+//         }
+//     }
+//     return result;
+// }
+// const userArray = [NaN, 1, 12, 0, -1, undefined];
+// const arrayResult = deleteInvalids(userArray);
+// console.log(arrayResult); 
 
 
 // // problem- 4
@@ -67,9 +72,41 @@ console.log(arrayResult);
 // }
 
 // const Info = { 
-//     name: "kolimuddin", 
+//     name: "kolimuddin@", 
 //     birthYear: 1999,
-//      siteName: "google" 
+//      siteName: "Google#" 
 //     };
 // const makingPass = password(Info); 
 // console.log(makingPass)
+
+// problem-5
+// function monthlySavings(payments, livingCost) {
+   
+//     if (!Array.isArray(payments) || typeof livingCost !== 'number') {
+//         return "Invalid input";
+//     }
+
+//     let totalEarning = 0;
+   
+//     for (let payment of payments) {
+      
+//         if (payment >= 3000) {
+//             totalEarning += payment * 20 / 100; 
+//         } else {
+//             totalEarning += payment;
+//         }
+//     }
+
+   
+//     let savingsAmount = totalEarning - livingCost;
+
+    
+//     if (savingsAmount <= 0) {
+//         return "earn more";
+//     } else {
+//         return savingsAmount; 
+//     }
+// }
+
+
+// console.log(monthlySavings([ 1000 , 2000 , 2500 ] , 5000)); 
